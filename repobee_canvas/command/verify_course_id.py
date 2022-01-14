049612140e5ae06009ed14ccf47ec795d74ff6e8
Jingjing Wang <j.wang8@student.tue.nl>
2022-01-14 19:10:55 +0100

    1.verify url, token, course id and get group set in verification. 2.change group set to combo

--- repobee_canvas/command/verify_course_id.py
+++ repobee_canvas/command/verify_course_id.py
@@ -6,18 +6,41 @@ from ..canvas_api.course        import Course
 
 from ..common                   import inform, warn, fault
 
-def VerifyCourseID(
+def VerifyCourseByID(
     canvas_base_url: str,
     canvas_access_token: str,
-    canvas_course_id: int) -> str:
+    canvas_course_id: int) -> (str, list):
     """Command to create a Canvas-Git mapping table and write it to a file."""
     CanvasAPI().setup(canvas_base_url, canvas_access_token)
 
+    course_name = getCourseName(canvas_course_id)
+    group_set = getGroupCategories(canvas_course_id)
+    return course_name, group_set
+
+def getCourseName(canvas_course_id: int)->str:
     try:
         course_info = CanvasAPI().course(canvas_course_id)
     except Exception as e:
-        fault(e)
         if "Unauthorized" in str(e):
-            warn("Repobee-canvas was not authorized to access your Canvas information. Please check the tooltip of the access token in the Settings Window.")
+            fault("Verifying Base URL and Access Token: failed")
+        elif "Not Found" in str(e):
+            fault("Verifying Course ID: failed ")
+        else:
+            fault(e)
     else:
+        inform("Verifying Base URL and Access Token: successful")
+        inform("Verifying  Course ID: successful")
         return course_info["name"]
+
+def getGroupCategories(canvas_course_id: int)->list:
+    try:
+        group_info = CanvasAPI().group_categories_per_course(canvas_course_id)
+    except Exception as e:
+        if "Not Found" in str(e):
+            fault("Verifying Group Set: failed")
+    else:
+        inform("Verifying Group Set: successful")
+        group_set = []
+        for group in group_info:
+            group_set.append(group["name"])
+        return group_set
