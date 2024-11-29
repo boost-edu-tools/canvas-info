3d91c02e987f104068e40d55956a531e88374a64
Martijn Goorden <m.a.goorden@tue.nl>
2024-11-29 16:31:08 +0100

    Refactor create_students_files

--- repobee_canvas/command/create_students_files.py
+++ repobee_canvas/command/create_students_files.py
@@ -8,7 +8,7 @@ from canvasapi import Canvas
 from canvasapi.course import Course
 from canvasapi.paginated_list import PaginatedList
 
-from ..canvas_git_map import canvas_git_map_table_wizard
+from ..canvas_git_map import canvas_git_map_table_wizard, Table
 from ..common import fault, inform, warn
 from ..gui import KEY_EMAIL, KEY_GIT_ID, KEY_MEM_BOTH
 
@@ -16,24 +16,25 @@ GROUP = "group"
 EMAIL2GIT = "email2git"
 GROUP_SIZE = 3  # Only groups with this number of students will be included in the YAML file
 
+
 def CreateStudentsFiles(
-    canvas_base_url: str,
-    canvas_access_token: str,
-    canvas_course_id: int,
-    student_csv_info_file: str | None = None,
-    student_xlsx_info_file: str | None = None,
-    students_yaml_file: str | None = None,
-    students_teammates_file: str | None = None,
-    student_member_option: str = "email",
-    include_group: bool = False,
-    include_member: bool = False,
-    include_initials: bool = False,
+        canvas_base_url: str,
+        canvas_access_token: str,
+        canvas_course_id: int,
+        student_csv_info_file: str | None = None,
+        student_xlsx_info_file: str | None = None,
+        students_yaml_file: str | None = None,
+        students_teammates_file: str | None = None,
+        student_member_option: str = "email",
+        include_group: bool = False,
+        include_member: bool = False,
+        include_initials: bool = False,
 ):
     if (
-        not student_csv_info_file
-        and not student_xlsx_info_file
-        and not students_yaml_file
-        and not students_teammates_file
+            not student_csv_info_file
+            and not student_xlsx_info_file
+            and not students_yaml_file
+            and not students_teammates_file
     ):
         return
 
@@ -72,93 +73,8 @@ def CreateStudentsFiles(
                         )
 
                     if students_yaml_file:
-                        group_submissions = {}
-                        groupless_submissions = []
-                        smallgroup_submissions = []
-                        for info in canvas_git_mapping_table.get_stu_info():
-                            group = info[GROUP]
-                            if group:
-                                if group in group_submissions:
-                                    group_submissions[group][EMAIL2GIT].update(
-                                        info[EMAIL2GIT]
-                                    )
-                                else:
-                                    group_submissions[group] = info
-                            else:
-                                groupless_submissions.append(info)
-
-                        # Students who are not assigned to a group in a group assignment are
-                        # ignored by default when creating a students file. Most likely these
-                        # students are not actively participating in the course or have not yet
-                        # assigned themselves to a group. However, with parameter
-                        # include_groupless_students, you can override this default behavior.
-                        if len(group_submissions) == 0:
-                            warn(
-                                (
-                                    "No group submissions found for this group assignment. "
-                                    "Please run command 'repobee canvas prepare-assignment' to "
-                                    "resolve this issue. Or configure this assignment as an "
-                                    "individual assignment."
-                                )
-                            )
-
-                        for group in list(group_submissions.keys()):
-                            if group.members_count < group.max_membership:
-                                # Group is too small, remove it from the list
-                                for email in list(group_submissions[group][EMAIL2GIT]):
-                                    smallgroup_submissions.append(email)
-                                del group_submissions[group]
-
-                        inform("Create students YAML file...")
-
-                        if student_member_option == KEY_MEM_BOTH:
-                            write_yaml_file(
-                                students_yaml_file,
-                                group_submissions,
-                                include_group,
-                                include_member,
-                                include_initials,
-                                True,
-                                True
-                            )
-                        elif student_member_option == KEY_EMAIL:
-                            write_yaml_file(
-                                students_yaml_file,
-                                group_submissions,
-                                include_group,
-                                include_member,
-                                include_initials,
-                                True,
-                                False
-                            )
-
-                        elif student_member_option == KEY_GIT_ID:
-                            write_yaml_file(
-                                students_yaml_file,
-                                group_submissions,
-                                include_group,
-                                include_member,
-                                include_initials,
-                                False,
-                                True
-                            )
-
-                        else:
-                            fault("Invalid member option.")
-                            return
-
-                        inform(f"Created students YAML file: {students_yaml_file}.")
-
-                        n = len(groupless_submissions)
-                        inform(f"The following {n} students were not in a group:")
-                        for submission in groupless_submissions:
-                            email = list(submission[EMAIL2GIT].keys())
-                            inform(email[0])
-
-                        m = len(smallgroup_submissions)
-                        inform(f"The following {m} students were in too small groups, no repository made for them:")
-                        for student in smallgroup_submissions:
-                            inform(student)
+                        create_yaml_file(canvas_git_mapping_table, students_yaml_file, student_member_option,
+                                         include_group, include_member, include_initials)
 
                     if students_teammates_file:
                         canvas_git_mapping_table.writeTeammatesExcel(
@@ -172,14 +88,111 @@ def CreateStudentsFiles(
         fault("Non-existing Course ID")
 
 
+def create_yaml_file(
+        canvas_git_mapping_table: Table,
+        students_yaml_file: str | None = None,
+        student_member_option: str = "email",
+        include_group: bool = False,
+        include_member: bool = False,
+        include_initials: bool = False,
+):
+    group_submissions = {}
+    groupless_submissions = []
+    smallgroup_submissions = []
+    for info in canvas_git_mapping_table.get_stu_info():
+        group = info[GROUP]
+        if group:
+            if group in group_submissions:
+                group_submissions[group][EMAIL2GIT].update(
+                    info[EMAIL2GIT]
+                )
+            else:
+                group_submissions[group] = info
+        else:
+            groupless_submissions.append(info)
+
+    # Students who are not assigned to a group in a group assignment are
+    # ignored by default when creating a students file. Most likely these
+    # students are not actively participating in the course or have not yet
+    # assigned themselves to a group. However, with parameter
+    # include_groupless_students, you can override this default behavior.
+    if len(group_submissions) == 0:
+        warn(
+            (
+                "No group submissions found for this group assignment. "
+                "Please run command 'repobee canvas prepare-assignment' to "
+                "resolve this issue. Or configure this assignment as an "
+                "individual assignment."
+            )
+        )
+
+    for group in list(group_submissions.keys()):
+        if group.members_count < group.max_membership:
+            # Group is too small, remove it from the list
+            for email in list(group_submissions[group][EMAIL2GIT]):
+                smallgroup_submissions.append(email)
+            del group_submissions[group]
+
+    inform("Create students YAML file...")
+
+    if student_member_option == KEY_MEM_BOTH:
+        write_yaml_file(
+            students_yaml_file,
+            group_submissions,
+            include_group,
+            include_member,
+            include_initials,
+            True,
+            True
+        )
+    elif student_member_option == KEY_EMAIL:
+        write_yaml_file(
+            students_yaml_file,
+            group_submissions,
+            include_group,
+            include_member,
+            include_initials,
+            True,
+            False
+        )
+
+    elif student_member_option == KEY_GIT_ID:
+        write_yaml_file(
+            students_yaml_file,
+            group_submissions,
+            include_group,
+            include_member,
+            include_initials,
+            False,
+            True
+        )
+
+    else:
+        fault("Invalid member option.")
+        return
+
+    inform(f"Created students YAML file: {students_yaml_file}.")
+
+    n = len(groupless_submissions)
+    inform(f"The following {n} students were not in a group:")
+    for submission in groupless_submissions:
+        email = list(submission[EMAIL2GIT].keys())
+        inform(email[0])
+
+    m = len(smallgroup_submissions)
+    inform(f"The following {m} students were in too small groups, no repository made for them:")
+    for student in smallgroup_submissions:
+        inform(student)
+
+
 def write_yaml_file(
-    students_yaml_file: str,
-    group_submissions: dict,
-    include_group: bool,
-    include_member: bool,
-    include_initials: bool,
-    by_email: bool,
-    by_gitid: bool,
+        students_yaml_file: str,
+        group_submissions: dict,
+        include_group: bool,
+        include_member: bool,
+        include_initials: bool,
+        by_email: bool,
+        by_gitid: bool,
 ):
     sorted_group_submissions = {k: v for k, v in sorted(group_submissions.items(), key=lambda x: x[1][GROUP].name)}
     with Path(students_yaml_file).open("w") as outfile:
