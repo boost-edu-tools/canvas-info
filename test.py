5c4bc63d17f453be088d5acca116e519366575b3
Martijn Goorden <m.a.goorden@tue.nl>
2025-05-07 11:18:06 +0200

    Add connection test script

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+r = requests.get('https://api.github.com/events')
+r = requests.put('https://httpbin.org/put', data={'key': 'value'})
+r = requests.delete('https://httpbin.org/delete')
+r = requests.head('https://httpbin.org/get')
+r = requests.options('https://httpbin.org/get')
+
+s = requests.Session()
+
+s.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
+r = s.get('https://httpbin.org/cookies')
+
+print(r.text)
